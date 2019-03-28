92819e45ad0c4d19fbbc5398d33b54204fb8a320
JerecTharen <kf7ags@gmail.com>
2019-03-28 13:11:32 -0600

    worked on course select

--- vue-golf/src/App.vue
+++ vue-golf/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <CourseSelect />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CourseSelect from './components/CourseSelect';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CourseSelect
   }
 }
 </script>
--- vue-golf/src/components/CourseSelect.vue
+++ vue-golf/src/components/CourseSelect.vue
@@ -1,10 +1,37 @@
 <template>
-    
+    <div>
+        <h1>Course Select</h1>
+        <select v-model="course">
+            <option v-for="acourse in courses" v-bind:key="acourse.id" v-bind:value="acourse.id">{{acourse.name}}</option>
+        </select>
+        <button v-on:click="goToCourse">GO</button>
+    </div>
 </template>
 
 <script>
+
+    import axios from 'axios';
+
     export default {
-        name: "CourseSelect"
+        name: "CourseSelect",
+        created(){
+            axios.get('https://golf-courses-api.herokuapp.com/courses')
+                .then(response => {
+                    console.log(response.data.courses);
+                    this.courses = response.data.courses;
+                });
+        },
+        data(){
+            return {
+                courses: [],
+                course: '',
+            }
+        },
+        methods: {
+            goToCourse(){
+                console.log(this.course);
+            }
+        },
     }
 </script>
 
